5319b19488a8110166f4ece60f771762604186eb
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-11-14 17:16:20 +0100

    First commit

---
+++ src/main/java/novi/nl/Les11BESpringbootmodelhuiswerk/Les11BeSpringbootModelHuiswerkApplication.java
@@ -0,0 +1,13 @@
+package novi.nl.Les11BESpringbootmodelhuiswerk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les11BeSpringbootModelHuiswerkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Les11BeSpringbootModelHuiswerkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novi/nl/Les11BESpringbootmodelhuiswerk/Les11BeSpringbootModelHuiswerkApplicationTests.java
@@ -0,0 +1,13 @@
+package novi.nl.Les11BESpringbootmodelhuiswerk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les11BeSpringbootModelHuiswerkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
