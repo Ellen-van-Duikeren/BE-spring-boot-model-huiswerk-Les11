c7952ddbe6b7df7ffba5cd60b22deb28c256a4cf
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-11-21 13:53:28 +0100

    Added put and delete

---
+++ src/main/java/novi/nl/Les11BESpringbootmodelhuiswerk/controllers/TelevisionController.java
@@ -0,0 +1,70 @@
+package novi.nl.Les11BESpringbootmodelhuiswerk.controllers;
+
+import novi.nl.Les11BESpringbootmodelhuiswerk.exceptions.ResourceNotFoundException;
+import novi.nl.Les11BESpringbootmodelhuiswerk.models.Television;
+import novi.nl.Les11BESpringbootmodelhuiswerk.repositories.TelevisionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/televisions")
+
+public class TelevisionController {
+    @Autowired
+    private TelevisionRepository repos;
+
+    @GetMapping("")
+    public ResponseEntity<Iterable<Television>> getTelevisions() {
+        return ResponseEntity.ok(repos.findAll());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<String> createTelevision(@RequestBody Television television) {
+        Television savedTelevision = repos.save(television);
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/televisions/" + savedTelevision.getId()).toUriString());
+        return ResponseEntity.created(uri).body("Television saved");
+    }
+
+    @GetMapping("/pricesUnder")
+    public ResponseEntity<Iterable<Television>> getTelevisionsUnderPrice(@RequestParam double price) {
+        return ResponseEntity.ok(repos.findByPriceLessThan(price));
+    }
+
+    @GetMapping("/byBrand")
+    public ResponseEntity<Iterable<Television>> getTelevisionsByBrand(@RequestParam String brand) {
+        return ResponseEntity.ok(repos.findByBrand(brand));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Television> updateTelevision(@PathVariable long id, @RequestBody Television television) {
+        Optional<Television> updateTelevision = repos.findById(id);
+        if (updateTelevision.isPresent()) {
+            Television television1 = updateTelevision.get();
+            television1.setName(television.getName());
+            repos.save(television1);
+            return ResponseEntity.ok(television1);
+        } else {
+            throw new ResourceNotFoundException("Television not found");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTelevision(@PathVariable long id) {
+        Optional<Television> television = repos.findById(id);
+        if (television.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            Television television1 = television.get();
+            repos.delete(television1);
+            return ResponseEntity.ok("Television deleted");
+        }
+    }
+}
+
+
--- src/main/java/novi/nl/Les11BESpringbootmodelhuiswerk/repositories/TelevisionRepository.java
+++ src/main/java/novi/nl/Les11BESpringbootmodelhuiswerk/repositories/TelevisionRepository.java
@@ -1,2 +1,19 @@
-package novi.nl.Les11BESpringbootmodelhuiswerk.repositories;public class TelevisionRepository {
+package novi.nl.Les11BESpringbootmodelhuiswerk.repositories;
+
+import novi.nl.Les11BESpringbootmodelhuiswerk.models.Television;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+    public Iterable<Television> findByPriceLessThan(double price);
+    public Iterable<Television> findByBrand(String brand);
+
+
 }
